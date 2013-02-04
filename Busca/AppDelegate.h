a82e41c9bf8a7eb63df0955b0d8b304ca0816656
protecsip <asalas@protecmedia.com>
2013-02-04 17:29:00 +0100

    Initial Commit

---
+++ Busca/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Busca
+//
+//  Created by Alberto on 04/02/13.
+//  Copyright (c) 2013 Protecmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Busca/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Busca
+//
+//  Created by Alberto on 04/02/13.
+//  Copyright (c) 2013 Protecmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
